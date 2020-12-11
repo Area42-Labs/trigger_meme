7eb80efeed36cb5b3bb732e5809d144c85aabf32
Fedora <61968330+Forenche@users.noreply.github.com>
2020-12-11 14:42:03 +0530

    build.sh: update repo link

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/stormbreaker-project/kernel_xiaomi_surya/ -b  alpha  kernel
+git clone --depth=1 https://gitlab.com/Forenche/https-github.com-stormbreaker-project-kernel_xiaomi_surya -b  master  kernel
 cd kernel
 git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
 git clone --depth=1 https://github.com/stormbreaker-project/AnyKernel3 -b surya AnyKernel
