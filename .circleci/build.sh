7ec2a7c4cf7512884ad5d1de85492537379b4696
Rakool <61968330+Forenche@users.noreply.github.com>
2021-01-17 15:06:24 +0530

    build.sh: update for realmeme

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 https://gitlab.com/Forenche/https-github.com-stormbreaker-project-kernel_xiaomi_surya -b  master  kernel
+git clone --depth=1 https://gitlab.com/Forenche/android_kernel_rmx1851 -b  reb  kernel
 cd kernel
 git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
-git clone --depth=1 https://github.com/stormbreaker-project/AnyKernel3 -b surya AnyKernel
+git clone --depth=1 https://github.com/stormbreaker-project/AnyKernel3 -b rmx1851 AnyKernel
 git clone --depth=1 https://android.googlesource.com/platform/system/libufdt libufdt
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%F-%S")
 START=$(date +"%s")
-export CONFIG_PATH=$PWD/arch/arm64/configs/surya-perf_defconfig
+export CONFIG_PATH=$PWD/arch/arm64/configs/RMX1851_defconfig
 PATH="${PWD}/clang/bin:$PATH"
 export ARCH=arm64
 export KBUILD_BUILD_HOST=circleci
@@ -26,7 +26,7 @@ function sendinfo() {
         -d chat_id="$chat_id" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
-        -d text="<b>• surya-Stormbreaker Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Poco x3</b> (surya)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>Proton clang 12</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #AOSP-Alpha"
+        -d text="<b>• realmeme-Stormbreaker-EOL Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Realmeme 3 Pro</b> (RMX1851)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>Proton clang 12</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #AOSP-Alpha"
 }
 # Push kernel to channel
 function push() {
@@ -36,7 +36,7 @@ function push() {
         -F chat_id="$chat_id" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
-        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Poco x3 (surya) for AOSP-Alpha</b> | <b>Proton clang 12</b>"
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Realmeme 3 Pro (RMX1851)</b> | <b>Proton clang 12</b>"
 }
 # Fin Error
 function finerr() {
@@ -49,7 +49,7 @@ function finerr() {
 }
 # Compile plox
 function compile() {
-   make O=out ARCH=arm64 surya-perf_defconfig
+   make O=out ARCH=arm64 RMX1851_defconfig
        make -j$(nproc --all) O=out \
                              ARCH=arm64 \
 			     CC=clang \
@@ -66,7 +66,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 surya-Stormbreaker-${TANGGAL}.zip *
+    zip -r9 realmeme-Stormbreaker-EOL-${TANGGAL}.zip *
     cd .. 
 }
 sticker
